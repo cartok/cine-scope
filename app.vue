b033d598d6fe239d226aba30dc4011769de96092
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2025-01-07 17:00:24 +0100

    wip

--- app.vue
+++ app.vue
@@ -14,6 +14,23 @@
 import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
+// TOOD: Does not help, got to fix the font flashs -> Download and compress fonts... serve static.
+useHead({
+  link: [
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+    {
+      href: 'https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,slnt,wdth,wght,GRAD,YTAS,YTFI,YTLC,YTUC@8..144,-10..0,25..151,100..1000,97,649..854,715,416..570,528..760&display=block',
+      rel: 'stylesheet',
+    },
+    {
+      rel: 'preload',
+      as: 'font',
+      href: 'https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,slnt,wdth,wght,GRAD,YTAS,YTFI,YTLC,YTUC@8..144,-10..0,25..151,100..1000,97,649..854,715,416..570,528..760&display=block',
+    },
+  ],
+})
+
 const route = useRoute()
 const skipLinks = ref()
 
