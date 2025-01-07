f9848983c8f0f33e26f730d9be6ff149fb60a385
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2025-01-07 17:05:51 +0100

    fix

--- pages/discover.vue
+++ pages/discover.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <header>
+    <header class="g-typography">
       <h1 class="g-text-title g-text-title-large">Welcome</h1>
       <p class="g-text-body g-text-body-medium">Here will be sections</p>
     </header>
